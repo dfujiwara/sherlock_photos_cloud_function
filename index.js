4b2a798b6192559edfbc1ab242e4deba86e30d1a
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2018-05-06 15:15:38 -0700

    Initial implementation fetching the photo link from Dropbox
    Note that we are using the fetch API internally in the Dropbox API.
    
    To get to the temporarly photo link we have to use two APIs from
    dropbox.
    
    1. list_folder
    2. get_temporary_link

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
+let Dropbox = require('dropbox').Dropbox
+require('isomorphic-fetch')
+
 /**
  * This is the google cloud function entry point fro Sherlock Photos App. 
- * @param {*} req 
- * @param {*} res 
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
  */
-
 exports.getPhotoURL = (req, res) => {
     switch (req.method) {
         case 'GET':
-            res.send('This would be the url')
+            getFiles()
+                .then((url) => {
+                    res.send('This is the url: ' + url)
+                })
+                .catch((reason) => {
+                    console.error('Failure reason: ' + reason)
+                    res.status(500).end()
+                })
             break
         default:
             res.status(405).end()
     }
+}
+
+const getFiles = () => {
+    return new Promise((resolve, reject) => {
+        let dbx = new Dropbox({
+            accessToken: 'YOUR_ACCESS_TOKEN_HERE'
+        })
+        dbx.filesListFolder({
+                path: '/apps/sherlock_photos'
+            })
+            .then((response) => {
+                const paths = response.entries.map((metadataEntry) => {
+                    return metadataEntry.path_lower
+                })
+                if (paths.length === 0) {
+                    reject('none found')
+                    return
+                }
+                const randomIndex = Math.floor(Math.random() * paths.length)
+                return dbx.filesGetTemporaryLink({
+                    path: paths[randomIndex]
+                })
+            })
+            .then((response) => {
+                resolve(response.link)
+            })
+            .catch((reason) => {
+                reject(reason)
+            })
+    })
 }
